ef98bd67bfad8bfbb4401f3ff22883439e14495b
jose <jalonsoriveiro@danielcastelao.org>
2020-03-08 10:48:05 -0700

    añadir colaborador y comentarios

--- src/main/java/Cdgit.java
+++ src/main/java/Cdgit.java
@@ -8,13 +8,15 @@ public class Cdgit {
 
 
     public static void main(String[] args) throws IOException {
-        // If you don't specify the GitHub user id then the sdk will retrieve it via /user endpoint
-// GitHub github = new GitHubBuilder().withOAuthToken("520e9f3a603e585eef77c305e437bb1d6adaaf23").build();
+
+        //crear el objeto github realizando la autenticación mediante un fichero con el token
         GitHub github =
                 GitHubBuilder.fromPropertyFile("/home/local/DANIELCASTELAO/jalonsoriveiro/IdeaProjects/progGit/src/main/java/.github").build();
+       //Creamos el repositorio
         GHRepository repo = github.createRepository(
                 "CDpruebatoken2","this is my new repository",
                 "https://github.com/jalonsoriveiro/",true/*public*/);
+        //añadimos colaborador
 //repo.addCollaborators(github.getUser("damiancastelao"));
     }
 }
