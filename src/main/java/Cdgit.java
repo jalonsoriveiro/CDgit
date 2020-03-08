3fb9cad2a80dfdcb00c887e569ba12aee16fefb0
jose <jalonsoriveiro@danielcastelao.org>
2020-03-08 09:46:56 -0700

    crear repo con usuario y contraseña

---
+++ src/main/java/Cdgit.java
@@ -0,0 +1,18 @@
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+
+import java.io.IOException;
+
+public class Cdgit {
+
+
+    public static void main(String[] args) throws IOException {
+            //GitHub github = GitHub.connect();
+            GitHub github = new GitHubBuilder().withPassword("jalonsoriveiro", "jos662056792").build();
+            GHRepository repo = github.createRepository(
+                    "new-repository34434","this is my new repository",
+                    "https://github.com/jalonsoriveiro/",true/*public*/);
+// repo.addCollaborators(github.getUser("jalonsoriveiro"),github.getUser("rtyler"));
+    }
+}
