63be5e0440a3f31f13069ac8f644f118c2ee4705
jose <jalonsoriveiro@danielcastelao.org>
2020-03-08 09:52:46 -0700

    crear repo con token en fichero

--- src/main/java/Cdgit.java
+++ src/main/java/Cdgit.java
@@ -9,10 +9,12 @@ public class Cdgit {
 
     public static void main(String[] args) throws IOException {
         // If you don't specify the GitHub user id then the sdk will retrieve it via /user endpoint
-        GitHub github = new GitHubBuilder().withOAuthToken("520e9f3a603e585eef77c305e437bb1d6adaaf23").build();
+// GitHub github = new GitHubBuilder().withOAuthToken("520e9f3a603e585eef77c305e437bb1d6adaaf23").build();
+        GitHub github =
+                GitHubBuilder.fromPropertyFile("/home/local/DANIELCASTELAO/jalonsoriveiro/IdeaProjects/progGit/src/main/java/.github").build();
         GHRepository repo = github.createRepository(
-                "new-repositorytoken","this is my new repository",
+                "CDpruebatoken2","this is my new repository",
                 "https://github.com/jalonsoriveiro/",true/*public*/);
-// repo.addCollaborators(github.getUser("abayer"),github.getUser("rtyler"));
+//repo.addCollaborators(github.getUser("damiancastelao"));
     }
 }
