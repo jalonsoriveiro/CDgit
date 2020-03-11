258a0d03a03838d78907e6336c2f236061ee8431
Jose Alonso <jalonsoriveiro@danielcastelao.org>
2020-03-11 14:31:48 +0100

    Examen clonar repositorio comentado

--- src/main/java/Cdgit.java
+++ src/main/java/Cdgit.java
@@ -12,11 +12,13 @@ public class Cdgit {
     public static void main(String[] args) throws IOException {
             //creamos el objeto GitHUb con las conexion realizada
             GitHub github = new GitHubBuilder().withPassword("jalonsoriveiro", "").build();
-            //Creamos el repositorio
+            //clonar repositorio
+        //Pedimos los valores necesarios para realizar el clonado
         String githubSourceUrl = Integer.parseInt(JOptionPane.showInputDialog('Dame tu repositorio'));
         String branchName = Integer.parseInt(JOptionPane.showInputDialog('rama'));
         String destinationFile = Integer.parseInt(JOptionPane.showInputDialog('url directorio destino'));
         int credentialsProvider = Integer.parseInt(JOptionPane.showInputDialog("credenciales"));
+       //Realizamos el clonado
         Git.cloneRepository().setURI(githubSourceUrl)
                 .setBranch(branchName)
                 .setDirectory(new File(destinationFile))
